4e90fc2376291a82d54371d4a409ef8d34c2bd77
0xkhan <0xkhan@protonmail.com>
2022-01-29 22:35:27 +0100

    Added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    context: path.resolve(__dirname, 'src'),
+    entry: './sassyflexgrid.scss',
+
+    module: {
+        rules: [
+            {
+                test: /.(scss|css)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            reloadAll: true
+                        }
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+}
